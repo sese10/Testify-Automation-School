f3a7b8d387924156c229ea0132df22f6c48f2c71
sese10 <109763742+sese10@users.noreply.github.com>
2022-10-03 16:01:34 +0200

    update

--- Javascript Programming Language/JS Code Challenge/codeChallenge10.js
+++
@@ -1,8 +0,0 @@
-//create a function that filters out negative numbers
-
-function GetPositiveNumber(numberArray ) {
-    const value = numberArray.filter(function(num) { return num > -1; });
-    console.log(value);
-}
-
-GetPositiveNumber([1, 81,4, -5, 40, 58]);
